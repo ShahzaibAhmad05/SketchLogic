2c4694d5711c086b312390f15ffb3c9df7a1f1d6
Shahzaib <shahzaibahmad6789@gmail.com>
2025-08-15 14:46:12 +0500

    Restructured repo

--- skelo_ai/__init__.py
+++
@@ -1,4 +0,0 @@
-# skelo_ai/__init__.py
-
-from .inference import SketchLogic
-__all__ = ["SketchLogic"]
---
+++ skelo_ai/wire_detector.py
@@ -0,0 +1,10 @@
+from skelo_ai import SketchLogic
+
+def detect_wires():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
