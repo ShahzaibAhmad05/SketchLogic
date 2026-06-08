9b4a28270c30adbd653f492756b02d6258b23e22
ShahzaibAhmad05 <shahzaibahmad6789@gmail.com>
2026-06-08 21:26:52 +0500

    wire everything into controller

--- sketchlogic/controller.py
+++ sketchlogic/controller.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import sketchlogic.model.controller
-# import sketchlogic.wiring.controller
+import sketchlogic.wiring.controller
+import sketchlogic.converter.controller
 import json
 
 
@@ -9,8 +10,11 @@ def run(input_image_path: Path, output_json_path: Path) -> None:
     Controller for the sketchlogic system.
     """
 
-    model_results = sketchlogic.model.controller.run(input_image_path, debug=True)
-    # circuit_json = sketchlogic.wiring.controller.run(input_image_path, model_results)
+    model_results, next_id = sketchlogic.model.controller.run(input_image_path, debug=True)
+    model_results, wires, io_results, next_id = sketchlogic.wiring.controller.run(
+        input_image_path, model_results, next_id
+    )
+    output = sketchlogic.converter.controller.run(model_results, wires, io_results)
 
     with open(output_json_path, "w") as file:
-        json.dump(model_results, file, indent=4)
+        json.dump(output, file, indent=4)
