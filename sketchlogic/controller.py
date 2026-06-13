49fc6e78155ba7e6387a33235b0c493115bd083c
pratyush <pratyush2582002@gmail.com>
2026-06-13 15:53:23 +0530

    ENH: Set Debug Flag from Command line Flag

--- sketchlogic/controller.py
+++ sketchlogic/controller.py
@@ -2,21 +2,22 @@ from pathlib import Path
 import sketchlogic.model.controller
 import sketchlogic.connector.controller
 import sketchlogic.converter.controller
+from sketchlogic.parser import parse_args
 import json
 
 
-def run(input_image_path: Path, output_json_path: Path) -> None:
+def run(input_image_path: Path, output_json_path: Path, debug: bool = False) -> None:
     """
     Controller for the sketchlogic system.
     """
 
-    model_results, next_id = sketchlogic.model.controller.run(input_image_path, debug=True)
+    model_results, next_id = sketchlogic.model.controller.run(input_image_path, debug=debug)
 
     model_results, wires, io_results, next_id = sketchlogic.connector.controller.run(
-        input_image_path, model_results, next_id, debug=True
+        input_image_path, model_results, next_id, debug=debug
     )
 
-    output = sketchlogic.converter.controller.run(model_results, wires, io_results, debug=True)
+    output = sketchlogic.converter.controller.run(model_results, wires, io_results, debug=debug)
 
     with open(output_json_path, "w") as file:
         json.dump(output, file, indent=4)
